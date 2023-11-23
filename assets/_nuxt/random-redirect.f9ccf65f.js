1f7704091cac73c38e69b2166e09464928dc5ce3
scott <scott5497ify@gmail.com>
2023-11-23 17:13:10 +0800

    Updates

--- assets/_nuxt/random-redirect.f9ccf65f.js
+++
@@ -1,1 +0,0 @@
-import{K as o,M as e}from"./entry.3d156dd3.js";const a=o(()=>{if(Math.random()>.5)return console.log("來自random-direct中間件，重新導向至 /count"),e("/count");console.log("啥都沒發生by 中間件")});export{a as default};
---
+++ assets/_nuxt/wintermelon.46750aef.js
@@ -0,0 +1,1 @@
+import"./entry.8f5f5651.js";const r=""+new URL("wintermelon.6e752d3a.png",import.meta.url).href;export{r as _};
