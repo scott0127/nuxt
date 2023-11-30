1e74d3b5963574e2ec2bd22c65c7e7167cd031ee
scott <scott5497ify@gmail.com>
2023-11-30 23:40:51 +0800

    Updates

---
+++ assets/_nuxt/random-redirect.e916704f.js
@@ -0,0 +1,1 @@
+import{g as o}from"./entry.117a19e1.js";import"./swiper-vue.797db16b.js";const r=o(()=>{Math.random()>.5&&console.log("來自random-direct中間件，重新導向至 /count"),console.log("啥都沒發生by 中間件")});export{r as default};
