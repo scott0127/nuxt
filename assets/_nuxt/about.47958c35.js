19984a624b6647e1cf56fd28179189cf249d6417
scott <scott5497ify@gmail.com>
2023-12-02 03:18:16 +0800

    Updates

--- assets/_nuxt/about.47958c35.js
+++
@@ -1,1 +0,0 @@
-import{N as t,C as s,O as e}from"./swiper-vue.797db16b.js";const o={class:"bg-white py-24"},a=e("div",{class:"flex flex-col items-center"},[e("h1",{class:"text-6xl font-semibold text-yellow-400"}," 大家好！我是 pakkkk "),e("p",{class:"my-8 text-3xl text-gray-600"}," 這裡是 /about ")],-1),c=[a],i={__name:"about",setup(l){return(_,n)=>(s(),t("div",o,c))}};export{i as default};
