da65d6815b868008e84e5c36879e2871266ddd9e
scott <scott5497ify@gmail.com>
2023-12-01 22:12:33 +0800

    Updates

--- assets/_nuxt/random-error-redirect.db157775.js
+++
@@ -1,1 +0,0 @@
-import{g as o,h as t}from"./entry.be2e67c1.js";import"./swiper-vue.797db16b.js";const r=o(()=>{if(Math.random()>.5)return console.log("來自random-direct中間件，重新導向至 /count"),t({statusCode:877,statusMessage:"不可以進入逮斯",fatal:!0});console.log("啥都沒發生by 中間件")});export{r as default};
---
+++ assets/_nuxt/wintermelon.ec9c7efd.js
@@ -0,0 +1,1 @@
+import"./entry.5a000d8a.js";const r=""+new URL("wintermelon.6e752d3a.png",import.meta.url).href;export{r as _};
