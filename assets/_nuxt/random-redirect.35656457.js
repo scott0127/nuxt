75552a8cf104766bef589152d46595fee5685182
scott <scott5497ify@gmail.com>
2023-12-05 01:08:52 +0800

    Updates

--- assets/_nuxt/random-redirect.35656457.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./entry.9626024a.js";import"./swiper-vue.afa238ab.js";const r=o(()=>{Math.random()>.5&&console.log("來自random-direct中間件，重新導向至 /count"),console.log("啥都沒發生by 中間件")});export{r as default};
---
+++ assets/_nuxt/wintermelon.d6f4a3fe.js
@@ -0,0 +1,1 @@
+import"./entry.3307e11c.js";const r=""+new URL("wintermelon.6e752d3a.png",import.meta.url).href;export{r as _};
